185b28459815e9fd5e1a5b8910e8ca08a5873891
manu <msrimat@umich.edu>
2019-04-03 11:03:44 -0400

    hello

--- fuzzer.py
+++ fuzzer.py
@@ -11,15 +11,15 @@ from subprocess import Popen, PIPE
 while True:
     tester = ""
     temp = ""
-    for x in range(77):
+    for x in range(12):
         temp = random.choice(string.printable)
         tester = tester + temp
     child = subprocess.Popen("./jsonParser", stdin=subprocess.PIPE, stderr=subprocess.PIPE)
     _, stdErrOut = child.communicate(input = tester)
     segFault = -11
     if child.returncode == segFault:
-        print "BROKEN FAILURE (%d): %s" % (child.returncode, s)
+        print "BROKEN FAILURE (%d): %s" % (child.returncode, tester)
         print "Base 64 Encoded segfault string:"
-        print base64.b64encode(s)
+        print base64.b64encode(tester)
         sys.exit(0);
 print("none found")
